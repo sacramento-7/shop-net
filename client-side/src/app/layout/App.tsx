c9fb0b346e17927cd523b3fb9c9c14f79e46bae0
sacramento-7 <hossein.kashfinia@gmail.com>
2023-09-11 17:17:31 +0330

    End of product detail page

--- client-side/src/app/layout/App.tsx
+++ client-side/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
+
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -24,7 +25,7 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
-        <Catalog />
+        <Outlet />
       </Container>
     </ThemeProvider>
   );
--- client-side/src/app/layout/Header.tsx
+++ client-side/src/app/layout/Header.tsx
@@ -1,18 +1,80 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
-interface Props{
+const midLinks = [
+    { title: 'Catalog', path: '/catalog' },
+    { title: 'About', path: '/about' },
+    { title: 'Contact', path: '/contact' }
+]
+
+const rightLinks = [
+    { title: 'Login', path: '/login' },
+    { title: 'Register', path: '/register' }
+]
+
+const navStyle = {
+    color: 'inherit',
+    typography: 'h6',
+    textDecoration: 'none',
+    '&:hover': {
+        color: 'grey.500'
+    },
+    '&.active': {
+        color: 'text.secondary'
+    }
+};
+
+interface Props {
     darkMode: boolean;
     handleThemeChange: () => void;
 }
 
-export default function Header({darkMode, handleThemeChange}: Props){
+export default function Header({ darkMode, handleThemeChange }: Props) {
     return (
-        <AppBar position="static" sx={{mb: 4}}>
-            <Toolbar>
-                <Typography variant="h6">
-                    Milan Sport
-                </Typography>
-                <Switch checked={darkMode} onChange={handleThemeChange}/>
+        <AppBar position="static" sx={{ mb: 4 }}>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Box display='flex' alignItems='center'>
+                    <Typography variant="h6" component={NavLink} to={"/"}
+                        sx={navStyle}
+                    >
+                        Milan Sport
+                    </Typography>
+                    <Switch checked={darkMode} onChange={handleThemeChange} />
+                </Box>
+
+                <List sx={{ display: 'flex' }}>
+                    {midLinks.map(({ title, path }) => (
+                        <ListItem
+                            component={NavLink}
+                            to={path}
+                            sx={navStyle}
+                        >
+                            {title.toUpperCase()}
+                        </ListItem>
+                    ))}
+                </List>
+
+                <Box display='flex' alignItems='center'>
+                    <IconButton size="large" edge='start' color="inherit" sx={{ mr: 2 }}>
+                        <Badge badgeContent="4" color="secondary">
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+
+                    <List sx={{ display: 'flex' }}>
+                        {rightLinks.map(({ title, path }) => (
+                            <ListItem
+                                component={NavLink}
+                                to={path}
+                                sx={navStyle}
+                            >
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+
             </Toolbar>
         </AppBar>
     )
