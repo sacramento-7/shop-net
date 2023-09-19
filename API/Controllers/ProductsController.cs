921788e7ce620a899ab8044b0855af9eb5be422f
sacramento-7 <hossein.kashfinia@gmail.com>
2023-09-19 17:52:05 +0330

    End of error handling

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,9 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController: ControllerBase
+    public class ProductsController: BaseApiController
     {
         
         private readonly StoreContext _context;
@@ -26,7 +24,10 @@ namespace API.Controllers
 
         [HttpGet("{id}")] // api/products/3
         public async Task<ActionResult<Product>> GetProduct(int id){
-            return await _context.Products.FindAsync(id);
+            var product = await _context.Products.FindAsync(id);
+
+            if(product == null) return NotFound();
+            return product;
         }
     }
 }
--- client-side/src/features/catalog/Catalog.tsx
+++ client-side/src/features/catalog/Catalog.tsx
@@ -1,3 +1,6 @@
+import { error } from "console";
+import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product"
 import ProductList from "./ProductList";
 import { useState, useEffect } from "react";
@@ -6,12 +9,16 @@ import { useState, useEffect } from "react";
 export default function Catalog() {
 
     const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-      fetch('https://localhost:7160/api/products')
-        .then(response => response.json())
-        .then(data => setProducts(data))
+      agent.Catalog.list()
+      .then(products => setProducts(products))
+      .catch(error => console.log(error))
+      .finally(() => setLoading(false))
     }, []); //this empty brucket makes the useEffect run once that is very important point!
+
+    if(loading) return <LoadingComponent message="Loading Products ..."/>
 
     return (
         <>
