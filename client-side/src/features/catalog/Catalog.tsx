e33c83c968929dc8a52f45d9a4139d7a2d310fb7
sacramento-7 <hossein.kashfinia@gmail.com>
2023-10-14 11:30:47 +0330

    End of filters and pagination

--- client-side/src/features/catalog/Catalog.tsx
+++ client-side/src/features/catalog/Catalog.tsx
@@ -2,26 +2,74 @@ import LoadingComponent from "../../app/layout/LoadingComponent";
 import ProductList from "./ProductList";
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { fetchProductsAsync, productSelectors } from "./catalogSlice";
+import { fetchFilters, fetchProductsAsync, productSelectors, setPageNumber, setProductParams } from "./catalogSlice";
+import { Grid, Paper } from "@mui/material";
+import ProductSearch from "./ProductSearch";
+import RadioButtonGroup from "../../app/components/RadioButtonGroup";
+import CheckboxButtons from "../../app/components/CheckboxButtons";
+import AppPagination from "../../app/components/AppPagination";
 
+const sortOptions = [
+    { value: 'name', label: 'Alphabetical' },
+    { value: 'priceDesc', label: 'Price - High to low' },
+    { value: 'price', label: 'Price - Low to high' }
+]
 
 export default function Catalog() {
-    //const [products, setProducts] = useState<Product[]>([]);
     const products = useAppSelector(productSelectors.selectAll);
-    const {productsLoaded, status} = useAppSelector(state => state.catalog);
+    const { productsLoaded, filtersLoaded, brands, types, productParams, metaData } = useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
-    //const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-      if(!productsLoaded) dispatch(fetchProductsAsync());
+        if (!productsLoaded) dispatch(fetchProductsAsync());
     }, [productsLoaded, dispatch]); // [] this empty brucket makes the useEffect run once that is very important point!
 
-    if(status.includes('pending')) return <LoadingComponent message="Loading Products ..."/>
+    useEffect(() => {
+        if (!filtersLoaded) dispatch(fetchFilters());
+    }, [dispatch, filtersLoaded]);
+
+    if (!filtersLoaded) return <LoadingComponent message="Loading Products ..." />
 
     return (
-        <>
-            <ProductList products={products} />
-        </>
+        <Grid container columnSpacing={4}>
+            <Grid item xs={3}>
+                <Paper sx={{ mb: 2 }}>
+                    <ProductSearch />
+                </Paper>
+                <Paper sx={{ mb: 2, p: 2 }}>
+                    <RadioButtonGroup
+                        selectedValue={productParams.orderBy}
+                        options={sortOptions}
+                        onChange={(e) => dispatch(setProductParams({ orderBy: e.target.value }))}
+                    />
+                </Paper>
+                <Paper sx={{ mb: 2, p: 2 }}>
+                    <CheckboxButtons
+                        items={brands}
+                        checked={productParams.brands}
+                        onChange={(items: string[]) => dispatch(setProductParams({ brands: items }))}
+                    />
+                </Paper>
+                <Paper sx={{ mb: 2, p: 2 }}>
+                    <CheckboxButtons
+                        items={types}
+                        checked={productParams.types}
+                        onChange={(items: string[]) => dispatch(setProductParams({ types: items }))}
+                    />
+                </Paper>
+            </Grid>
+            <Grid item xs={9}>
+                <ProductList products={products} />
+            </Grid>
+            <Grid item xs={3} />
+            <Grid item xs={9} sx={{mb:2, mt:2}}>
+                {metaData &&
+                <AppPagination
+                    metaData={metaData}
+                    onPageChange={(page: number) => dispatch(setPageNumber({ pageNumber: page }))}
+                />}
+            </Grid>
+        </Grid>
     )
 }
 
